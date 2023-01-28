063e5b215021929596c76a35015b1d89c5a6b6c9
Ricky <ricky@ricky.com>
2023-01-28 16:19:01 -0600

    Task 4 complete

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,44 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobID(){
+        Job test1 = new Job();
+        Job test2 = new Job();
+        assertNotEquals(test1, test2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job newJob = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertTrue(newJob.getName() instanceof String);
+        assertEquals(newJob.getName(), "Product Tester");
+        assertTrue(newJob.getEmployer() instanceof Employer);
+        assertEquals(newJob.getEmployer().getValue(), "ACME");
+        assertTrue(newJob.getLocation() instanceof Location);
+        assertEquals(newJob.getLocation().getValue(), "Desert");
+        assertTrue(newJob.getPositionType() instanceof PositionType);
+        assertEquals(newJob.getPositionType().getValue(), "Quality Control");
+        assertTrue(newJob.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(newJob.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job test1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        Job test2 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertFalse(test1.equals(test2));
+    }
 
 }
