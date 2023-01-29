df5fb1271f0705cabc40f20b0956e94efd8659f8
Ricky <ricky@ricky.com>
2023-01-28 19:00:07 -0600

    Task 5 complete

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Job {
 
@@ -32,6 +33,56 @@ public class Job {
 
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+//    public static void main(String[] args){
+//        Job test = new Job();
+//        boolean check = Stream.of(test.getName(), test.getEmployer(), test.getLocation(), test.getPositionType(), test.getCoreCompetency()).allMatch(Objects::isNull);
+//        System.out.println(check);
+//        System.out.println(test.getEmployer().getValue());
+//    }
+
+    @Override
+    public String toString() {
+        String output;
+        boolean check = Stream.of(this.getName(), this.getEmployer(), this.getLocation(), this.getPositionType(), this.getCoreCompetency()).allMatch(Objects::isNull);
+        if(check){
+            output = "\nOOPS! This job does not seem to exist.\n";
+        } else {
+            String errorMessage = "Data not available";
+            String part1 = String.format("ID: %s", this.getId());
+            String part2;
+            String part3;
+            String part4;
+            String part5;
+            String part6;
+            if (this.getName() != null) {
+                part2 = String.format("Name: %s", this.getName());
+            } else {
+                part2 = String.format("Name: %s", errorMessage);
+            }
+            if (this.getEmployer().getValue() != null) {
+                part3 = String.format("Employer: %s", this.getEmployer().getValue());
+            } else {
+                part3 = String.format("Employer: %s", errorMessage);
+            }
+            if (this.getLocation().getValue() != null) {
+                part4 = String.format("Location: %s", this.getLocation().getValue());
+            } else {
+                part4 = String.format("Location: %s", errorMessage);
+            }
+            if (this.getPositionType().getValue() != null) {
+                part5 = String.format("Position Type: %s", this.getPositionType().getValue());
+            } else {
+                part5 = String.format("Position Type: %s", errorMessage);
+            }
+            if (this.getCoreCompetency().getValue() != null) {
+                part6 = String.format("Core Competency: %s", this.getCoreCompetency().getValue());
+            } else {
+                part6 = String.format("Core Competency: %s", errorMessage);
+            }
+            output = "\n" + part1 + "\n" + part2 + "\n" + part3 + "\n" + part4 + "\n" + part5 + "\n" + part6 + "\n";
+        }
+        return output;
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -16,7 +16,7 @@ public class JobTest {
     public void testSettingJobID(){
         Job test1 = new Job();
         Job test2 = new Job();
-        assertNotEquals(test1, test2);
+        assertNotEquals(test1.getId(), test2.getId());
     }
 
     @Test
@@ -41,4 +41,48 @@ public class JobTest {
         assertFalse(test1.equals(test2));
     }
 
+    @Test
+    public void testToStringStartsAndEndsWithNewLine(){
+        Job test = new Job();
+        int lastChar = test.toString().length() - 1;
+        assertEquals(test.toString().charAt(0), '\n');
+        assertEquals(test.toString().charAt(lastChar), '\n');
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData(){
+        Job test1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        String part1 = String.format("ID: %s", test1.getId());
+        String part2 = String.format("Name: %s", test1.getName());
+        String part3 = String.format("Employer: %s", test1.getEmployer().getValue());
+        String part4 = String.format("Location: %s", test1.getLocation().getValue());
+        String part5 = String.format("Position Type: %s", test1.getPositionType().getValue());
+        String part6 = String.format("Core Competency: %s", test1.getCoreCompetency().getValue());
+        String expected = "\n"+part1+"\n"+part2+"\n"+part3+"\n"+part4+"\n"+part5+"\n"+part6+"\n";
+        assertEquals(test1.toString(), expected);
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField(){
+        Job test = new Job();
+        test.setEmployer(new Employer("PortaFab"));
+        test.setLocation(new Location("Chesterfield, MO"));
+        test.setPositionType(new PositionType("Slave"));
+        test.setCoreCompetency(new CoreCompetency("Obedience"));
+        String part1 = "ID: 1";
+        String part2 = "Name: Data not available";
+        String part3 = "Employer: PortaFab";
+        String part4 = "Location: Chesterfield, MO";
+        String part5 = "Position Type: Slave";
+        String part6 = "Core Competency: Obedience";
+        String expected = "\n"+part1+"\n"+part2+"\n"+part3+"\n"+part4+"\n"+part5+"\n"+part6+"\n";
+        assertEquals(test.toString(), expected);
+    }
+
+    @Test
+    public void testToStringHandlesEmptyObject(){
+        Job test555 = new Job();
+        String error = "\nOOPS! This job does not seem to exist.\n";
+        assertTrue(test555.toString() == error);
+    }
 }
